ece58faea801f600139bb1e8d271d87e7e72f63c
tanvtse183061-eng <tanvtse183061@fpt.edu.vn>
2025-10-23 14:42:37 +0700

    giao dien dashboard

--- DuanTopic1 - Copy/src/Pages/DealerStaff/Dashboard.jsx
+++ DuanTopic1 - Copy/src/Pages/DealerStaff/Dashboard.jsx
@@ -1,9 +1,32 @@
-import  './Dashboard.css';
+import './Dashboard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faUser, faCar, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 
 export default function Dashboard() {
+  const lists = [
+    { id: 1, icon: faCartShopping, color: '#3b82f6', bg: '#e0ecff', title: 'Tổng đơn hàng', number: '8', value: '+12%'},
+    { id: 2, icon: faUser, color: '#16a34a', bg: '#dcfce7', title: 'Khách hàng', number: '11', value: '+8%'},
+    { id: 3, icon: faCar, color: '#9333ea', bg: '#f3e8ff', title: 'Xe trong kho', number: '15', value: '-3%'},
+    { id: 4, icon: faMoneyBill, color: '#f59e0b', bg: '#fef3c7', title: 'Doanh thu', number: '0.0M VNĐ', value: '+15%' },
+  ];
+
   return (
-    <div className='dashboard'>
-      <h1>Hello</h1>
+    <div className="dashboardf">
+      <h1 style={{ fontSize: '20px' }}>Dashboard</h1>
+      <div className="dashboard-list">
+        {lists.map((list) => (
+          <div key={list.id} className="dash">
+            <div className="icon-box" style={{ background: list.bg, color: list.color }}>
+              <FontAwesomeIcon icon={list.icon} size="lg" />
+            </div>
+            <div className="number">{list.number}</div>
+            <div className="title">{list.title}</div>
+           
+              {list.value}
+            </div>
+         
+        ))}
+      </div>
     </div>
   );
 }
